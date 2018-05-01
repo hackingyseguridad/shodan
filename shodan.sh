f597a736e22a073084bf81f79e41c38c6cae1a2c
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-05-01 13:23:09 +0200

    Update shodan.sh

--- shodan.sh
+++ shodan.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # Script para testear IP con https://www.shodan.io/
-# Instalar shodan: easy_install shodan
+# Instalar shodan: pip install shodan
 # shodan init APIKEY
 # Lee del fichero ip.txt la lista de IP
 # Uso: sh shodan.sh
 # (C) hackingyseguridad.com 2017 
 echo  
-DATE=`date +%Y%m%d`
 for n in `cat ip.txt`; do echo $n; shodan host $n; done
-service networking restart
